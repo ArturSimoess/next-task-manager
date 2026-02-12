64e4bbba9ca5233d384b63d6cd3d8a1b28f2f639
Artur Simoes <artursimoess@gmail.com>
2026-02-11 23:01:24 -0300

    feat: tailwindcss included

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import './globals.css';
 import { Providers } from '@/lib/providers';
+
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind funcionando
+      </h1>
+
       <ul>
         <li>
           <Link href="/tasklist">Task List</Link>
